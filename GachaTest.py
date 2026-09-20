25a6dc1626aa227b7afcc04fa303df8b1be7adf5
Salvinoke <felixius.arnanda@gmail.com>
2026-09-20 12:54:54 +0700

    Updated GachaTest.py

--- GachaTest.py
+++ GachaTest.py
@@ -1,4 +1,6 @@
 import random as rand
+from colorama import init, Fore
+init(autoreset=True)
 
 pool = {
     "Legendary": .6,
@@ -15,14 +17,44 @@ for rarity in pool:
 
     TotalWeight += chances
 
-for i in range(10):
+while True:
+    try:
+        gachaType = int(input(Fore.CYAN + """Pick gacha method:
+[1] Single Pull
+[2] 10x Pull
+Input: """))
+
+        if gachaType not in [1,2]:
+            print(Fore.RED + "ERROR: Please input either 1 or 2!")
+            continue
+        
+    except ValueError:
+        print(Fore.RED + "ERROR: Input a valid value!")
+        continue
+    break
+
+print("="*50)
+
+if gachaType == 1:
     keyNum = rand.randint(0,int(TotalWeight))
     weight = 0
-
+    
     for rarity in pool:
         chances = pool[rarity]
         weight += chances
-
+    
         if keyNum <= weight:
             print(rarity)
             break
+else:
+    for i in range(10):
+        keyNum = rand.randint(0,int(TotalWeight))
+        weight = 0
+
+        for rarity in pool:
+            chances = pool[rarity]
+            weight += chances
+
+            if keyNum <= weight:
+                print(rarity)
+                break
