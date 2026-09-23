c2b27dbec41eb6e4c7ea89b514f7bac9c88d1642
Salvinoke <felixius.arnanda@gmail.com>
2026-09-23 21:41:34 +0700

    Working on Random.py

---
+++ Random.py
@@ -0,0 +1,17 @@
+import random as rand
+
+width = 20
+height = 11
+
+randomnum = rand.randint(1,height-2)
+randompos = rand.randint(-(width//2-1),(width//2-2))
+
+leftspace,rightspace = (width//2-1),(width//2-2)
+
+for i in range(height):
+    if i == 0 or i == height-1:
+        print("*"*width)
+    elif i == randomnum:
+        print("*"+" "*(leftspace+randompos) + "|" + " "*(rightspace-randompos) + "*")
+    else:
+        print("*" + " "*(width-2) + "*")
